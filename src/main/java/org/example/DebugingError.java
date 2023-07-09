6d45e7e6ed660a7ba35345883f2d2be9b139e4df
cos12378 <yrias8700@gmail.com>
2023-07-09 14:43:57 +0900

    review

--- src/main/java/org/example/DebugingError.java
+++ src/main/java/org/example/DebugingError.java
@@ -6,26 +6,31 @@ import java.util.Scanner;
 // Enter를 누르세요. 그러면 코드 내에서 공백 문자를 확인할 수 있습니다.
 public class DebugingError {
     public static void main(String[] args) {
+        //TODO: 배열에서 주워진 범위의 합을 구하는 프로그램을 구하시오.
+        //애매한 자료형은 long으로 선언하는 습관을 가지자
         Scanner sc = new Scanner(System.in);
-        int testcase = 1;
-        int answer = 0;
+        int testcase = 4;
+        long answer = 0;
         System.out.println("start");
-        int A[] = new int[100001];
-        int S[] = new int[100001];
+        long A[] = new long[100001];
+        long S[] = new long[100001];
 
         for(int i=1; i<100000; i++){
-            A[i] = (int) (Math.random() * Integer.MAX_VALUE);
+            A[i] = (long) (Math.random() * Integer.MAX_VALUE);
             S[i] = S[i-1] + A[i];
         }
 
         for(int t=1; t<testcase; t++){
-            int query = 1000;
+            int query = 10;
             for(int i = 0; i<query; i++){
-                int start = 1;
-                int end = 100;
+                int start = 2;
+                int end = 10;
                 answer += S[end] - S[start-1];
-                System.out.println(t + " " + answer);
+                System.out.println(S[10]+" "+S[1]);
+                System.out.println(S[10]-S[1]);
+                System.out.println(t + " : " + answer);
             }
+
         }
     }
 }
